4cc3bd03dd373aa34904afeb1ad5e9646964d7a2
AlexeyShimko <shimko390@mail.ru>
2022-05-25 13:42:05 +0300

    Initial commit

---
+++ src/main/java/MainServer/ServerStart.java
@@ -0,0 +1,8 @@
+package MainServer;
+
+public class ServerStart {
+    public static void main(String[] args) {
+        MainServer mainServer = new MainServer();
+        mainServer.start();
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module ru.geekbrains.chat {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+
+    requires org.kordamp.bootstrapfx.core;
+
+    opens ru.geekbrains.chat to javafx.fxml;
+    exports ru.geekbrains.chat;
+}
